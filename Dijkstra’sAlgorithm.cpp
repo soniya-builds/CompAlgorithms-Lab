85479d75c2c4a1c1e86365dce470bd1e5601305e
soniya-builds <soniyasmiles05@gmail.com>
2026-01-08 20:47:29 +0600

    Dijkstra’s Algorithm

---
+++ Dijkstra’sAlgorithm.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<pair<int,int>>> adj(V);
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+    }
+
+    int src;
+    cin >> src;
+
+    vector<int> dist(V, INT_MAX);
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+    dist[src] = 0;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top();
+        pq.pop();
+
+        if (d > dist[u]) continue;
+
+        for (auto [v, w] : adj[u]) {
+            if (dist[u] + w < dist[v]) {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++) {
+        if (dist[i] == INT_MAX) cout << "INF ";
+        else cout << dist[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
